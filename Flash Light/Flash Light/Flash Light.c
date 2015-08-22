dbf27eaedc8cf1f7228471cfcc2bfb21c7314d21
Bishal Heuju <b.heuju@yahoo.com>
2015-08-22 20:14:24 +0545

    Flash Light configured as SPI slave to receive 16 bit data for PWM value

--- Flash Light/Flash Light/Flash Light.c
+++ Flash Light/Flash Light/Flash Light.c
@@ -18,7 +18,9 @@
 		* Uses PWM to control the intensity of the LED
 	
 	Notes:
-		* ** TODO **
+		* Receives 16 bit data for PWM value
+		* Master needs to send two times 8 bit data
+		
 		
 	Operation Guidelines:
 		1. ** TODO **
@@ -30,6 +32,18 @@
 #define CLEARBIT(x, y) (x &= ~(_BV(y)))
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define SS		PINB2
+#define MOSI	PINB3
+#define MISO	PINB4
+#define SCK		PINB5
+
+volatile uint8_t dataHigh;
+volatile uint8_t dataLow;
+volatile uint16_t data = 1;
+
+uint8_t count = 1;
 
 void initPWM()
 {
@@ -39,21 +53,55 @@ void initPWM()
 	
 	TCCR1B |= (1<<CS10);	//No prescaler
 	
-	TCCR1B |= (1<<WGM13) | (1<<WGM12);	//Fast PWM
+	TCCR1B |= (1<<WGM13)|(1<<WGM12);	//Fast PWM
 	TCCR1A |= (1<<WGM11);	
 	
 	TCCR1A |= (1<<COM1A1);	//PWM non-inverting mode
 	
 	ICR1 = 5999;	//top value - for 150Hz PWM freq
-	OCR1A = 4000;	//higher value -> high duty cycle
+	OCR1A = 1;	//higher value -> high duty cycle
+}
+
+void initSpiSlave()
+{
+	DDRB |= (1<<MISO);	//MISO as o/p
+	DDRB &= ~((1<<MOSI)|(1<<SS)|(1<<SCK));	//MOSI, SCK and SS as i/p
+	
+	PORTB |= (1<<SS); //internal pull up SS;
+	
+	SPCR &= ~(1<<MSTR);	//set as slave
+	SPCR |= (1<<SPE)|(1<<SPIE);	//enable spi and spi interrupt
 }
 
 int main(void)
 {
 	initPWM();
+	initSpiSlave();
+	
+	sei();
 	
     while(1)
     {
-        
+		OCR1A = data;
     }
+}
+
+ISR(SPI_STC_vect)
+{
+	if (count == 1)
+	{
+		dataLow = SPDR;
+	}
+	else if (count == 2)
+	{
+		dataHigh = SPDR;
+		data = dataHigh;
+		
+		data = (data<<8) | dataLow;
+	}
+	count++;
+	if (count > 2)
+	{
+		count = 1;
+	}
 }
